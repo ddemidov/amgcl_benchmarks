7bce7c2d9fc9ab67594b886729b5166eded43f7b
Denis Demidov <dennis.demidov@gmail.com>
2018-04-04 14:57:32 +0300

    Update submit script

--- distr_mem/poisson/run_amgcl.py
+++ distr_mem/poisson/run_amgcl.py
@@ -11,13 +11,14 @@ parser.add_argument('--strong', dest='strong', action='store_true', default=Fals
 parser.add_argument('--const', dest='const', action='store_true', default=False)
 args = parser.parse_args(sys.argv[1:])
 
-script_name = f'scripts/amgcl_{args.np}x{args.omp}_{args.dpp}'
-params = '-p isolver.maxiter=500 local.coarse_enough=500 local.coarsening.aggr.eps_strong=0.0 -i cg -d spastix'
+script_name = 'scripts/amgcl_{np}x{omp}_{dpp}'.format(
+        np=args.np, omp=args.omp, dpp=args.dpp)
+params = 'isolver.maxiter=500 local.coarse_enough=500 local.coarsening.aggr.eps_strong=0.0 -i=cg -d=eigen_splu'
 
 if args.strong:
-    params += f' -n {args.dpp}'
+    params += ' -n={}'.format(args.dpp)
 else:
-    params += f' -n {int(0.5 + (args.np * args.omp * args.dpp)**(1/3))}'
+    params += ' -n={}'.format(int(0.5 + (args.np * args.omp * args.dpp)**(1/3)))
 
 if args.const:
     script_name += '_const'
@@ -26,24 +27,24 @@ if args.const:
 script_name += '.sbatch'
 
 open(script_name, 'w').write(
-f"""#!/bin/bash
+"""#!/bin/bash
 #SBATCH --job-name=amgcl
 #SBATCH -D .
 #SBATCH --output=logs/%x-%j.out
 #SBATCH --error=logs/%x-%j.err
-#SBATCH --cpus-per-task={args.omp}
-#SBATCH --ntasks={args.np}
+#SBATCH --cpus-per-task={omp}
+#SBATCH --ntasks={np}
 #SBATCH --time=00:10:00
 
-export OMP_NUM_THREADS={args.omp}
+export OMP_NUM_THREADS={omp}
 export OMP_WAIT_POLICY=ACTIVE
 
-if [ {args.omp} -gt 1 ]; then
+if [ {omp} -gt 1 ]; then
   export OMP_PLACES=sockets
 fi
 
 echo "params: {params}"
 srun ./dmem_poisson_amgcl {params}
-""")
+""".format(np=args.np, omp=args.omp, params=params))
 
 subprocess.Popen(['sbatch', script_name]).wait()
